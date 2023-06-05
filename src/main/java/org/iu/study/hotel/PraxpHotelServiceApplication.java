3198280e947dd94582bed8454c06367f46f3b588
Adrian Lübke <adrian.luebke@proton.me>
2023-06-05 17:10:46 +0200

    first commit

---
+++ src/main/java/org/iu/study/hotel/PraxpHotelServiceApplication.java
@@ -0,0 +1,13 @@
+package org.iu.study.hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraxpHotelServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PraxpHotelServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/iu/study/hotel/PraxpHotelServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.iu.study.hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraxpHotelServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
